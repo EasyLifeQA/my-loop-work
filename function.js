9ef662943ece4168fcb5008299afa3e93edac4e2
EasyLifeQA <agbenga247@gmail.com>
2024-10-02 22:18:06 +0100

    function task 1

--- function.js
+++ function.js
@@ -1,5 +1,17 @@
-let length = 9;
-let width = 5;{
-let area = length * width
-console.log( 'the area of triangle is '+   area)
+//let length = 9;
+//let width = 5;{
+//let area = length * width
+//console.log( 'the area of triangle is '+   area)
+//}
+
+function AreaOfRectangle (length, bregth) {
+    console.log(length * bregth)
+    const AreaOfRectangle = (length * bregth)
+    return AreaOfRectangle
 }
+AreaOfRectangle (6, 9)
+
+function ProductOfRectangle (length, width) {
+    console.log(length * width)
+}
+ProductOfRectangle(7, 3)
