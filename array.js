1c4fd9b71637ff61a1b65ca38fdfd3bd1e14def4
EasyLifeQA <agbenga247@gmail.com>
2024-10-02 23:06:44 +0100

    array task

---
+++ array.js
@@ -0,0 +1,6 @@
+const friends = ["paul" , "leke" , "yemi" , "segun" , "ope"]
+console.log(friends)
+const bestfriend = "tolu"
+console.log(bestfriend)
+console.log(friends [3])
+console.log(friends.length)
